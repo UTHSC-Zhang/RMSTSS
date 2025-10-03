b5d469bd93bf7518ff69ee64efd73ee3b3dac2f7
Arnab Aich <118152677+arnabaich96@users.noreply.github.com>
2025-10-02 22:59:06 -0400

    update
    up

--- RMSTSS.Rcheck/00_pkg_src/RMSTSS/R/zzz-imports.R
+++
@@ -1,6 +0,0 @@
-# R/zzz-imports.R
-# Centralized roxygen imports to satisfy R CMD check for non-exported helpers.
-
-#' @importFrom stats uniroot setNames
-#' @importFrom utils head tail
-NULL
--- RMSTSS.Rcheck/00_pkg_src/RMSTSS/inst/doc/RMSTSS-App.R
+++
@@ -1,6 +0,0 @@
-## ----setup, include=FALSE-----------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE,
- cache = TRUE,
-  cache.lazy = FALSE,   # materialize objects now (safer)
-  autodep = TRUE)
-
